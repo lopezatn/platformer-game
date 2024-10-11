012139078e1cabd83033ddacd455b64a45a46cf2
lopezatn <agustinlpzb@gmail.com>
2024-10-11 19:27:07 +0200

    Basic functionality components added, first rendering of Game Component

---
+++ src/components/game/Game.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useRef, useState } from 'react';
+// import Player from '../Player';
+// import Platform from '../Platform';
+// import Enemy from '../Enemy';
+import './Game.css';
+
+const Game = () => {
+    // Game state and logic here
+
+    return (
+        <div className='game-container'>
+            {/* Render game elements here */}
+            {/* <Player />
+            <Platform />
+            <Enemy /> */}
+        </div>
+    );
+};
+
+export default Game;
